84473ea0f45b6a5b535da5f548e3a3379cc34555
Kostyan <konstantingolubev8@gmail.com>
2021-03-31 22:32:12 +0300

    init commit

---
+++ Main.py
@@ -0,0 +1,22 @@
+import os
+import re
+from itertools import filterfalse
+
+pattern = ".*XmlUtil.*|.*NrdInboxProcessingServiceImpl.*"
+
+
+with os.scandir('.') as entries:
+    for entry in entries:
+        if entry.is_file():
+            if '.txt' not in entry.name:
+                break
+            print('Start refinement on ' + entry.name)
+            with open(entry, 'r') as r:
+                data = r.read()
+                splitlines = data.splitlines()
+                regexp = re.compile(pattern)
+                filtered = list(filterfalse(lambda line: regexp.search(line), splitlines))
+                data = '\n'.join(filtered)
+                with open(entry, 'w') as w:
+                    w.write(data)
+                    print('Finish refinement on ' + entry.name)
